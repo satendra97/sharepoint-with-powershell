abc6e615a5361160e7b8f8de96077f9a49f8de67
satendra97 <60088760+satendra97@users.noreply.github.com>
2020-06-18 10:58:19 +0530

    Add files via upload

---
+++ getting user and their groups and exporting it to a csv file using powershell.ps1
@@ -0,0 +1,20 @@
+﻿Connect-SPOService -Url https://contoso-admin.sharepoint.com
+$st = Get-SPOUser -Site https://contoso.sharepoint.com/sites/contoso
+$objectCollection=@()  
+
+
+foreach ( $sts in $st)
+{#$object = New-Object PSObject 
+    foreach ($tt in $sts.Groups)
+        {
+            $object = New-Object PSObject 
+            #write-Host $sts.DisplayName,$sts.LoginName,$sts.UserType,$tt
+            $object | Add-Member -type NoteProperty -Name DISPLAYNAME -Value $sts.DisplayName
+            $object | Add-Member -type NoteProperty -Name LOGINNAMES  -Value $sts.LoginName  
+            $object | Add-Member -type NoteProperty -Name USERTYPES -Value $sts.UserType 
+            $object | Add-Member -type NoteProperty -Name GROUPS -Value $tt 
+        $objectCollection += $object
+         }
+
+ }
+ $objectCollection | Export-Csv C:\datafile.csv -NoTypeInformation
